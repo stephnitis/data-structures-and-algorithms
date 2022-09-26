e371729428dcfce05ccc72f10c77742a2db94ab9
Stephanie Hill <stephni.hill@gmail.com>
2022-09-25 20:56:01 -0700

    two tests

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -7,7 +7,26 @@ describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
+
+  it('Can successfully instantiate an empty linked list', () => {
+    expect(new LinkedList()).toBeInstanceOf(LinkedList);
+  });
+
+  it('Can properly insert into the linked list', () => {
+    let list = new LinkedList();
+    list.add(1);
+    expect.objectContaining({value: 1});
+  });
+
 });
+
+// Can successfully instantiate an empty linked list
+// Can properly insert into the linked list
+// The head property will properly point to the first node in the linked list
+// Can properly insert multiple nodes into the linked list
+// Will return true when finding a value within the linked list that exists
+// Will return false when searching for a value in the linked list that does not exist
+// Can properly return a collection of all the values that exist in the linked list
 
 
 //toBeInstanceOf(Class) <<
