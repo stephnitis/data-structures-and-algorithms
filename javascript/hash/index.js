4e4728c579e836f1706088eaf279690a5ee8043b
Stephanie Hill <stephni.hill@gmail.com>
2022-11-02 21:36:41 -0700

    oy

--- javascript/hash/index.js
+++ javascript/hash/index.js
@@ -3,12 +3,12 @@
 const LinkedList = require('../linked-list/index');
 
 class Hash {
-  constructor(size){
+  constructor(size) {
     this.size = size;
     this.buckets = new Array(size);
   }
 
-  hash(key){
+  hash(key) {
     let characters = key.split('');
     let asciiSum = characters.reduce((sum, character) => {
       return sum + character.charCodeAt(0);
@@ -18,11 +18,11 @@ class Hash {
     return initialHash % this.size;
   }
 
-  set(key, value){
+  set(key, value) {
     let position = this.hash(key);
-    let data = {[key]: value};
+    let data = { [key]: value };
 
-    if(this.buckets[position]){
+    if (this.buckets[position]) {
       let bucket = this.buckets[position];
       bucket.add(data);
     } else {
@@ -32,33 +32,44 @@ class Hash {
     }
   }
 
-  get(key){
+  get(key) {
     let index = this.hash(key);
 
-    if(this.buckets[index]){
+    if (this.buckets[index]) {
       let bucket = this.buckets[index];
+      console.log('bucket ---->', bucket);
+      let current = bucket.head;
+      while (current) {
+        console.log(current.value);
+        // if (key === current.keys()[0]) {
+        //   return current.value[key];
+        // }
+        current = current.next;
+      }
 
       let value = bucket.head.value[key];
       return value;
     }
   }
 
-  has(key){
-    if(this.get(key)){
+  has(key) {
+    if (this.get(key)) {
       return true;
     } else {
       return false;
     }
   }
 
-  keys(){
+  keys() {
     let keysArray = [];
-    for (let i = 0; i < this.buckets.length; i++){
-      if(this.buckets[i]){
-        for(let j= 0; j < this.buckets[i].length; j++){
-          if(!keysArray.includes(this.buckets[i][j][0])){
-            keysArray.push(this.buckets[i][j][0]);
-          }
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i]) {
+        let bucket = this.buckets[i];
+        let current = bucket.head;
+        while (current) {
+          console.log(Object.keys(current.value)[0]);
+          keysArray.push(Object.keys(current.value)[0]);
+          current = current.next;
         }
       }
     }
@@ -67,37 +78,25 @@ class Hash {
 
 }
 
-// map takes an array and function as argument
-
-// function map(arr, mapFunc) {
-//   const mapArr = [];
-//   // empty array
-//   // loop though array
-//   for(let i=0;i<arr.length;i++) {
-//     const result = mapFunc(arr[i], i, arr);
-//     mapArr.push(result);
-//   }
-//   return mapArr;
-// }
 
 let hashTable = new Hash(1024);
 
-console.log(hashTable.hash('potato'));
-console.log(hashTable.hash('banana'));
-console.log(hashTable.hash('starch monster'));
+// console.log(hashTable.hash('potato'));
+// console.log(hashTable.hash('banana'));
+// console.log(hashTable.hash('starch monster'));
 
+hashTable.set('potato', { style: 'french fried', rating: 10 });
+hashTable.set('potato', { style: 'chipped', rating: 7 });
+hashTable.set('starch monster', { name: 'Stephanie', hungry: true });
+hashTable.set('papaya', 'fruit');
+hashTable.set('Kali', 'pup');
+// console.log(hashTable);
+// console.log(JSON.stringify(hashTable.buckets[849]));
+// console.log(JSON.stringify(hashTable.buckets[971]));
 
-hashTable.set('potato', {style: 'french fried', rating: 10});
-hashTable.set('potato', {style: 'chipped', rating: 7});
-hashTable.set('starch monster', {name: 'Stephanie', hungry: true});
-console.log(hashTable);
-console.log(JSON.stringify(hashTable.buckets[849]));
-console.log(JSON.stringify(hashTable.buckets[971]));
+hashTable.get('potato');
 
-console.log('does the table have potato ----->', hashTable.has('potato'));
-console.log('does the table have linguini ----->', hashTable.has('linguini'));
+// console.log('does the table have potato ----->', hashTable.has('potato'));
+// console.log('does the table have linguini ----->', hashTable.has('linguini'));
 
-// hashTable.keys();
 console.log(hashTable.keys());
-
-module.exports = Hash;
