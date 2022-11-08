8fb047d594454cfc73925a9f431edaa0ada07cec
Stephanie Hill <stephni.hill@gmail.com>
2022-11-07 21:52:46 -0800

    code

--- javascript/graphs/README.md
+++ javascript/graphs/README.md
@@ -43,6 +43,21 @@ size:
 - Returns the total number of nodes in the graph
 - 0 if there are none
 
+### Branch Name: graph-breadth-first
+
+### Breadth First Features
+
+- breadth first
+- Arguments: Node
+- Return: A collection of nodes in the order they were visited.
+- Display the collection
+
 #### References
 
 - [Demo Code](https://github.com/codefellows/seattle-code-javascript-401d48/blob/main/class-35/inclass-demo/index.js)
+- [Geeks for Geeks](https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/)
+- [code fellows](https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Code_401/class-35/resources/graphs.html)
+- [GRAPH THEORY (DATA STRUCTURE)](https://seeve.medium.com/graph-theory-data-structure-89c7423de878)
+- [Learn JavaScript Graph Data Structure](https://jarednielsen.com/data-structure-graph-javascript/)
+- [Graph Data Structures in JavaScript for Beginners](https://adrianmejia.com/data-structures-for-beginners-graphs-time-complexity-tutorial/)
+- [Friend Luis](https://github.com/RosalesJr/data-structures-and-algorithms)
--- javascript/graphs/index.js
+++ javascript/graphs/index.js
@@ -6,8 +6,6 @@ class Vertex {
   }
 }
 
-// connects one node to another
-// directed edge has a starting and ending vertex
 class Edge {
   constructor(vertex, weight = 0) {
     this.vertex = vertex;
@@ -19,62 +17,90 @@ class Graph {
   constructor() {
     this.adjacencyList = new Map();
   }
-  //vertex stores something
+
   addVertex(value) {
     const vertex = new Vertex(value);
-    // takes vertex and a place to store edges
-    //key is the vertex or the node itself and the value will become its edges
     this.adjacencyList.set(vertex, []);
+    return vertex;
   }
 
-  addDirectedEdge(startVertex, endVertex) {
-    //want to find the node we want to connect to
-    //based on the start we will grab the starting vertex
-    //the key points us to the vertex, and getting it at that hash returns the array
-    //thus neighbors is an array
-    const neighbors = this.adjacencyList.get(startVertex);
-    neighbors.push(new Edge(endVertex));
+  addEdge(startVertex, endVertex, weight){
+    if(!this.adjacencyList.has(startVertex) || !this.adjacencyList.has(endVertex)){
+      throw new Error('Invalid Vertex');
+    }
+    const adjacencies = this.adjacencyList.get(startVertex);
+    adjacencies.push(new Edge(endVertex, weight));
+  }
+
+  // addDirectedEdge(startVertex, endVertex) {
+  //   const neighbors = this.adjacencyList.get(startVertex);
+  //   neighbors.push(new Edge(endVertex));
+  // }
+
+  getNodes(){
+    let nodes = [];
+    for(const [vertex, edge] of this.adjacencyList.entries()) {
+      nodes.push(vertex);
+    }
   }
 
   getNeighbors(vertex) {
-    // returns a deep copy to preserve data
+    if(!this.adjacencyList.has(vertex)){
+      throw new Error('Invalid Vertex');
+    }
     return [...this.adjacencyList.get(vertex)];
   }
 
-  // breadth first traversal
-  // if you don't pass in a callback, it gives you a function, otherwise if you do pass one it will use that function
-  // breadthFirst(root, callback = () => {}){
-  //otherwise use the callback in if statement
-  breadthFirst(root, callback) {
-    const queue = [root];
-    //behavior that mimics a queue in that array
-    const visited = new Set();
-    visited.add(root);
-    let current = null;
-    while (queue.length) {
-      current = queue.pop();
+  breadthFirst(startNode){
+    if(!startNode){
+      return null;
+    }
+    const queue = [];
+    const visitedNodes = new Set();
 
-      if (callback) callback(current.value);
-
-      //grab neighbors for block party if they exist
-      //remember, neighbors is an array of all of the edges
-      const neighbors = this.getNeighbors(current);
-      //in doing this each element in the neighbors array can be referred to as edge
-      //strengthen your array knowledge bitch
-      for (let edge of neighbors) {
-        //conditional
-        if (!visited.has(edge.vertex)) {
-          //edge is a specific node in our array
-          //edge has a property of vertex
-          visited.add(edge.vertex);
-          //put into queue so we can keep track of what we have visited
-          //unshift will put into the front of the array
-          queue.unshift(edge.vertex);
+    queue.push(startNode);
+    visitedNodes.add(startNode);
+    while(queue.length){
+      const currentNode = queue.shift();
+      const neighbors = this.getNeighbors(currentNode);
+      for(let neighbor of neighbors){
+        const neighborNode = neighbor.vertex;
+        if(visitedNodes.has(neighborNode)){
+          continue;
+        } else {
+          visitedNodes.add(neighborNode);
         }
+        queue.push(neighborNode);
       }
     }
-    //visited is a set
-    return visited;
+    return visitedNodes;
+  }
+
+  // breadthFirst(root, callback) {
+  //   const queue = [root];
+  //   const visited = new Set();
+  //   visited.add(root);
+  //   let current = null;
+  //   while (queue.length) {
+  //     current = queue.pop();
+  //     if (callback) callback(current.value);
+  //     const neighbors = this.getNeighbors(current);
+  //     for (let edge of neighbors) {
+  //       if (!visited.has(edge.vertex)) {
+  //         queue.unshift(edge.vertex);
+  //       }
+  //     }
+  //   }
+  //   return visited;
+  // }
+
+  size(){
+    let size = 0;
+    for(let key of this.adjacencyList.keys()){
+      size++;
+    }
+    return size;
   }
 }
 
+module.exports = Graph;
