420aa4d407482c3b462993fc0929171411da31a4
Stephanie Hill <stephni.hill@gmail.com>
2022-11-12 10:17:50 -0800

    roman to arabic number

---
+++ javascript/hash/challenges/romanToArabic.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function romanToArabic(number) {
+  //implement romanNumber table here
+  //must be in descending order for this code to work though ???
+  let romanNumber = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let roman = '';
+
+  for (let key in romanNumber) {
+    while (number >= romanNumber[key]) {
+      roman += key;
+      number -= romanNumber[key];
+
+    }
+  }
+  return roman;
+
+}
+
+console.log(romanToArabic(25));
